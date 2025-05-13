c65611a8296dc2b37a08188cacd47cdc309bd43c
kalina01255@naver.com <kalina01255@naver.com>
2025-05-14 00:05:50 +0900

    janhyuk_boj문제

--- Janghyuk/Week3/src/janghyuk_boj11004_k번째수.java
+++ Janghyuk/Week3/src/janghyuk_boj11004_k번째수.java
@@ -1,7 +1,7 @@
 import java.util.*;
 import java.io.*;
 
-public class Main {
+public class janghyuj_boj11004_k번째수 {
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
