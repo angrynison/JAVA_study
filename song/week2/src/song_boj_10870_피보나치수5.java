c09c053e0d097741f4446eb50df39efd59b02b6e
songhyunjeong <songhyunjeong@gmail.com>
2025-05-15 05:55:53 +0900

    song_boj_10870_피보나치수5.java

---
+++ song/week2/src/song_boj_10870_피보나치수5.java
@@ -0,0 +1,23 @@
+/*
+ * 링크: https://www.acmicpc.net/problem/10870
+ * 레벨: 브론즈 Ⅱ
+ * 키워드: 재귀
+ * 문제 요약:
+ *   - 피보나치 수열
+ *   - F(n) = F(n-1) + F(n-2)
+ */
+import java.util.*;
+
+public class song_boj_10870_피보나치수5 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.println(fibo(n));
+    }
+
+    private static int fibo(int n) {
+        if (n == 0) return 0;
+        if (n == 1) return 1;
+        return fibo(n - 1) + fibo(n - 2);
+    }
+}
