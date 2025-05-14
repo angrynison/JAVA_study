159e65f55c7480e79c62fefc4708b6a283a637e4
songhyunjeong <songhyunjeong@gmail.com>
2025-05-15 03:43:21 +0900

    song_pgs_HIndex.java

--- song/week3/src/song_pgs_HIndex.java
+++ song/week3/src/song_pgs_HIndex.java
@@ -1,19 +1,20 @@
 /*
  * 링크: https://school.programmers.co.kr/learn/courses/30/lessons/42747
  * 레벨: Lv.2
- * 태그: 정렬
+ * 키워드: 정렬
  * 문제 요약:
  *   - 인용 횟수가 h회 이상인 논문이 h편 이상인 h의 최댓값 구하기
  */
 import java.util.*;
 
 public class song_pgs_HIndex {
-    public static int solution(int[] citations) {
+    private static int solution(int[] citations) {
         // 인용 횟수 오름차순 정렬
         Arrays.sort(citations);
 
         for (int i = 0; i < citations.length; i++) {
-            int h = citations.length - i;  // h번 이상 인용된 논문 수
+            int h = citations.length - i;  // 현재 위치의 논문보다 많이 인용된 논문의 수
+            // 현재 위치의 논문보다 많이 인용된 논문의 수가 현재 위치의 논문의 인용 횟수보다 크면
             if (citations[i] >= h) {
                 return h;
             }
